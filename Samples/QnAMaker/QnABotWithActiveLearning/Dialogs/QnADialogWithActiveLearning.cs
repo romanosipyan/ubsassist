8be24caf3d6b0ea76a9cd2eb32e05999022f2361
Maciek <macpawluk@gmail.com>
2018-09-06 19:18:01 +0200

    some settings

--- Samples/QnAMaker/QnABotWithActiveLearning/Dialogs/QnADialogWithActiveLearning.cs
+++ Samples/QnAMaker/QnABotWithActiveLearning/Dialogs/QnADialogWithActiveLearning.cs
@@ -5,8 +5,13 @@ namespace QnABotWithActiveLearning.Dialogs
 {
     // Train API only works with the QnAMaker V2 APIs: https://aka.ms/qnamaker-v2-apis
     [Serializable]
-    [QnAMaker("set yout subscription key here", "set your kbid here", "I don't understand this right now! Try another query!", 0.50, 3)]
-    public class QnADialogWithActiveLearning : QnAMakerDialog
+	[QnAMaker(
+		"90b4ce22-2b4a-49d2-b074-c796ccf48c80", 
+		"68c9242b-3835-41d8-9836-39eb09c87c7f", 
+		endpointHostName: "https://qnaubsassist.azurewebsites.net/qnamaker", 
+		scoreThreshold: 0.5, 
+		top: 3)]
+	public class QnADialogWithActiveLearning : QnAMakerDialog
     {
     }
 }
